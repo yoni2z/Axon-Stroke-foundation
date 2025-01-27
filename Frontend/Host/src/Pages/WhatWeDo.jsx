1a98951fd4b56878abfd0256fd1963b0221009bb
yoni2z <zyonas626@gmail.com>
2025-01-27 15:16:52 +0300

    added program and projects database and some changes on projects pictures

--- Frontend/Host/src/Pages/WhatWeDo.jsx
+++ Frontend/Host/src/Pages/WhatWeDo.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import TitleBanner from "../components/TitleBanner";
 //Components
 import Cards from "../components/What We Do/Cards";
-import VolunteerBg from "../assets/volunteer-title-bg.jpg";
+import VolunteerBg1 from "../assets/volunteer-title-bg.jpg";
+import VolunteerBg2 from "../assets/blog2.png";
+import VolunteerBg3 from "../assets/blog1.png";
 
 const WhatWeDo = () => {
   return (
     <div className="font-poppins">
-      <TitleBanner title="WHAT WE DO" backgroundImage={VolunteerBg} />
+      <TitleBanner title="WHAT WE DO" backgroundImage={VolunteerBg1} />
 
       <div className="py-[95px] mx-10">
         <div className="mb-[77px] text-center">
@@ -26,19 +28,19 @@ const WhatWeDo = () => {
         {/* Render What We Do Cards */}
         <div className="flex flex-col gap-14 mx-auto">
           <Cards
-            photo={VolunteerBg}
+            photo={VolunteerBg2}
             title="A. Stroke Awareness Program"
             description="Our Stroke Awareness Program aims to increase public knowledge about stroke prevention, symptoms, and the importance of early intervention."
             id="1"
           />
           <Cards
-            photo={VolunteerBg}
+            photo={VolunteerBg2}
             title="Free Stroke Therapy Programs"
             description="Our Free Stroke Therapy Programs provide essential rehabilitation services for stroke survivors, focusing on improving physical and cognitive recovery through therapy and support."
             id="2"
           />
           <Cards
-            photo={VolunteerBg}
+            photo={VolunteerBg3}
             title="Stroke Education & Research Programs"
             description="Our Stroke Education & Research Programs focus on advancing the knowledge of stroke, its prevention, and innovative treatments. We are committed to supporting research and spreading vital information to both the public and healthcare professionals."
             id="3"
