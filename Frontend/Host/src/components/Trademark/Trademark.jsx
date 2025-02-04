832d92294994cd87ba9caea971ca738faf4485bf
yoni2z <zyonas626@gmail.com>
2025-02-04 12:16:04 +0300

    improvement based on suggestion

--- Frontend/Host/src/components/Trademark/Trademark.jsx
+++ Frontend/Host/src/components/Trademark/Trademark.jsx
@@ -4,7 +4,10 @@ import "./styles.css";
 export const Trademark = () => {
   return (
     <div className="trademark">
-      <p>Copyright © Hope 2025. All Rights Reserved</p>
+      <p>
+        Copyright © Axon Stroke Foundation {new Date().getFullYear()}. All
+        Rights Reserved
+      </p>
     </div>
   );
 };
